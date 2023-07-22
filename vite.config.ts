cbc32dee824a03f27bb77c610fb7ab32798645b6
H3lltronik <esau.egs1@gmail.com>
2023-07-21 21:22:13 -0600

    router

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'assets'
+  publicDir: 'assets',
+  resolve: {
+    alias: {
+        '@': path.resolve(__dirname, './src'),
+      }
+  },
 })
