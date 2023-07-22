b3ef4eea721f823fc76ddd8983bb026946e6de4c
H3lltronik <esau.egs1@gmail.com>
2023-07-21 21:05:49 -0600

    prettier and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,34 @@ import {
   DesktopOutlined,
   FileOutlined,
   PieChartOutlined,
+  PoweroffOutlined,
   TeamOutlined,
   UserOutlined,
-} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import React, { useState } from 'react';
-import logo from './assets/nutras-logo.png';
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import {
+  Avatar,
+  Breadcrumb,
+  Button,
+  Layout,
+  Menu,
+  Popover,
+  Typography,
+  theme,
+} from "antd";
+import React, { useState } from "react";
+import logo from "./assets/nutras-logo.png";
 
+const { Text } = Typography;
 const { Header, Content, Footer, Sider } = Layout;
 
-type MenuItem = Required<MenuProps>['items'][number];
+type MenuItem = Required<MenuProps>["items"][number];
 
 function getItem(
   label: React.ReactNode,
   key: React.Key,
   icon?: React.ReactNode,
-  children?: MenuItem[],
+  children?: MenuItem[]
 ): MenuItem {
   return {
     key,
@@ -29,31 +40,59 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-  getItem('Produccion', '1', <PieChartOutlined />, [
-    getItem('OT', '100'),
-    getItem('Explosión de insumos', '2'),
-    getItem('Requisiciones', '3'),
-    getItem('Inventario', '4'),
+  getItem("Produccion", "1", <PieChartOutlined />, [
+    getItem("OT", "100"),
+    getItem("Explosión de insumos", "2"),
+    getItem("Requisiciones", "3"),
+    getItem("Inventario", "4"),
   ]),
-  getItem('Almacen', '20', <DesktopOutlined />, [
-    getItem('Inventario', '5'),
-    getItem('Movimientos', '6'),
+  getItem("Almacen", "20", <DesktopOutlined />, [
+    getItem("Inventario", "5"),
+    getItem("Movimientos", "6"),
   ]),
-  getItem('Direccion', 'sub1', <UserOutlined />, [
-    getItem('Solicitud trabajo', '7'),
-    getItem('Orden de trabajo', '8'),
-    getItem('Requisiciones', '9'),
+  getItem("Direccion", "sub1", <UserOutlined />, [
+    getItem("Solicitud trabajo", "7"),
+    getItem("Orden de trabajo", "8"),
+    getItem("Requisiciones", "9"),
   ]),
-  getItem('Calidad', 'sub3', <UserOutlined />, [
-    getItem('Revisiones de movimientos', '10'),
-    getItem('Revisiones de productos', '11'),
-    getItem('Inspecciones de área', '12'),
+  getItem("Calidad", "sub3", <UserOutlined />, [
+    getItem("Revisiones de movimientos", "10"),
+    getItem("Revisiones de productos", "11"),
+    getItem("Inspecciones de área", "12"),
   ]),
-  getItem('Compras', 'sub2', <TeamOutlined />, [
-    getItem('Ordenes compra', '13'), 
+  getItem("Compras", "sub2", <TeamOutlined />, [
+    getItem("Ordenes compra", "13"),
   ]),
-  getItem('Transporte', '14', <FileOutlined />),
+  getItem("Transporte", "14", <FileOutlined />),
 ];
+
+const popupContent = () => (
+  <>
+    <div className="w-min-[350px] bg-blue-200 p-5 flex gap-4">
+      <Avatar
+        className="bg-green-300 outline outline-green-900"
+        shape="square"
+        size={50}>
+        <strong className="text-green-900">AV</strong>
+      </Avatar>
+      <div className="my-auto max-w-[200px]">
+        <Text strong className="text-lg" ellipsis={true}>
+          Andrea Valenzuela
+        </Text>
+      </div>
+    </div>
+    <div className="flex px-3 py-2 gap-2">
+      <Button block>
+        <UserOutlined />
+        Mi perfil
+      </Button>
+      <Button block>
+        <PoweroffOutlined />
+        Cerrar sesión
+      </Button>
+    </div>
+  </>
+);
 
 const App: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -62,23 +101,63 @@ const App: React.FC = () => {
   } = theme.useToken();
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="demo-logo-vertical" style={{backgroundImage: `url(${logo})`}} />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}>
+        <div
+          className="demo-logo-vertical"
+          style={{ backgroundImage: `url(${logo})` }}
+        />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Orden de trabajo</Breadcrumb.Item>
+        <Header style={{ background: colorBgContainer }}>
+          <div className="w-full flex flex-row">
+            <nav className="flex-1">
+              <ul>
+                <li>Home</li>
+              </ul>
+            </nav>
+            <section className="flex items-center gap-3 flex-nowrap">
+              <Text>
+                Hola, <strong>Andrea</strong>
+              </Text>
+              <Popover
+                trigger={"click"}
+                content={popupContent()}
+                className="cursor-pointer">
+                <Avatar className="bg-green-300" shape="square" size={35}>
+                  <strong className="text-green-900">AV</strong>
+                </Avatar>
+              </Popover>
+            </section>
+          </div>
+        </Header>
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Orden de trabajoa</Breadcrumb.Item>
             <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
           </Breadcrumb>
-          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+          <div
+            className=""
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}>
             Orden de trabajo
           </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
       </Layout>
     </Layout>
   );
