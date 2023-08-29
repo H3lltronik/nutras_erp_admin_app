f68067d585436bd6ce141a49ef04331068361d88
Antonio Romano <aromanomacias@gmail.com>
2023-08-29 17:41:32 -0600

    login 2.0  with info modal

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,13 @@
 // Login.js
-import { Button, Form, Input } from "antd";
-import { useEffect } from "react";
+import { Button, Form, Input, Modal } from "antd";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthAPI } from "../../api";
 import useAuth from "../../hooks/useAuth";
 import { showToast } from "../../lib/notify";
-import { LoginParticles } from "./LoginParticles";
+import './styles.css'
+import logo from './../../assets/nutras-logo.png';
+import products from './../../assets/nutras-login-products.png';
 
 type FormValues = {
   username: string;
@@ -14,6 +16,7 @@ type FormValues = {
 
 function Login() {
   const { isAuthenticated, loading } = useAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -38,45 +41,102 @@ function Login() {
     console.log("result LOGIN", result);
   };
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-400">
-      <LoginParticles />
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-400 bg-gradient">
+      {/* <LoginParticles /> */}
 
-      <div className="z-10 p-8 bg-white shadow-md rounded">
-        <h2 className="text-2xl mb-4">Login</h2>
+      <div className="z-10 px-8 py-3 bg-white shadow-md rounded-2xl">
         <Form name="login" className="login-form" onFinish={onFinish}>
+          <div className="flex justify-center self-stretch">
+            <img className="" style={{maxWidth: '100%'}} src={logo} width={300} />
+          </div>
           <Form.Item
             name="username"
             rules={[
               {
                 required: true,
-                message: "Please input your Username!",
+                message: "Por favor ingresa tu correo electrónico!",
               },
             ]}>
-            <Input placeholder="Username" />
+            <Input placeholder="Correo electrónico" />
           </Form.Item>
           <Form.Item
             name="password"
             rules={[
               {
                 required: true,
-                message: "Please input your Password!",
+                message: "Por favor ingresa tu contraseña!",
               },
             ]}>
-            <Input.Password placeholder="Password" />
+            <Input.Password placeholder="Contraseña" />
           </Form.Item>
           <Form.Item>
             <Button
               type="primary"
               htmlType="submit"
-              className="w-full bg-green-600">
-              Log in
+              className="w-full"
+              style={{backgroundColor: '#44b44a'}}>
+              Iniciar Sesión
             </Button>
           </Form.Item>
+          <div className="flex flex-col items-center self-stretch mb-3">
+            <div className="w-10/12 text-center">
+              ¿Tienes problemas para acceder? Por favor envía un correo a
+            </div>
+            <div className="w-10/12 text-center">
+              <a className="underline text-blue-500 hover:underline" href="mailto:support@unnamedcomputerclub.com">support@unnamedcomputerclub.com</a>
+            </div>
+          </div>
+          <div className="flex flex-col items-center self-stretch mb-3">
+            <div className="w-10/12 text-center">
+              <a href="#">
+                ¿Deseas cambiar tu contraseña?
+              </a>
+            </div>
+          </div>
+          <div className="w-100 text-center">
+            <Button type="default" onClick={showModal}>
+              Información
+            </Button>
+          </div>
         </Form>
       </div>
+      <div className="flex items-center justify-center self-stretch">
+        <img style={{width: 600, maxWidth: '100%'}} src={products} />
+      </div>
+      <Modal title="Información" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} style={{top: 50}}
+      footer={[
+        <Button key="submit" type="default" loading={loading} onClick={handleOk}>
+          Ok
+        </Button>,
+      ]}>
+        <div className="flex flex-col justify-center items-center">
+          <span>
+            Developed with {'<3'} by <span className="italic">Unnamed Computer Club</span>®
+          </span>
+          <span className="flex flex-col justify-center items-center gap-1">
+            <a href="mailto:support@unnamedcomputerclub.com">support@unnamedcomputerclub.com</a> <a href="mailto:ventas@unnamedcomputerclub.com">ventas@unnamedcomputerclub.com</a>
+          </span>
+          <span>
+            Tel: +52 33 1785 9804
+          </span>
+          <span className="font-semibold">Versión del Software: 1.0</span>
+        </div>
+      </Modal>
     </div>
   );
 }
